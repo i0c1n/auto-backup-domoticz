62c59cef7bba3913da59cf5d67a64a1af543123b
i0c1n <samuel.bernicot@posteo.net>
2022-10-29 22:06:30 +0200

    Update example-backup.sh

--- example-backup.sh
+++ example-backup.sh
@@ -1,14 +1,14 @@
-if [ -e /mnt/backup/backup1.db ]; then
+if [ -e /mnt/backup/lundi.db ]; then
        echo "file found!"
 
 else
        echo "Téléchargement de la base..."
-       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/backup1.db
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/lundi.db
 
-
+fi 
+find mnt/backup/lundi.db -mtime +7 rm {} \;
 
 exit
-
 
 
 
