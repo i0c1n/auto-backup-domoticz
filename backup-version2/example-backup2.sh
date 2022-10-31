fa41a269a61238effb33dbc9eaa9880a82bc9bf9
i0c1n <samuel.bernicot@posteo.net>
2022-10-31 13:26:08 +0100

    Add files via upload

---
+++ backup-version2/example-backup2.sh
@@ -0,0 +1,329 @@
+if [ -e /mnt/backup/jour1.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour1.db
+
+fi 
+
+
+if [ -e /mnt/backup/jour2.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour2.db
+
+fi 
+
+
+
+if [ -e /mnt/backup/jour3.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour3.db
+
+fi 
+
+
+
+if [ -e /mnt/backup/jour4.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour4.db
+
+fi 
+
+
+
+if [ -e /mnt/backup/jour5.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour5.db
+
+fi 
+
+
+
+
+if [ -e /mnt/backup/jour6.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour6.db
+
+fi 
+
+
+
+
+if [ -e /mnt/backup/jour7.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour7.db
+
+fi 
+
+
+if [ -e /mnt/backup/jour8.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour8.db
+
+fi 
+
+
+
+if [ -e /mnt/backup/jour9.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour9.db
+
+fi 
+
+
+
+if [ -e /mnt/backup/jour10.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour10.db
+
+fi 
+
+
+if [ -e /mnt/backup/jour11.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour11.db
+
+fi 
+
+
+if [ -e /mnt/backup/jour12.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour12.db
+
+fi 
+
+
+if [ -e /mnt/backup/jour13.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour13.db
+
+fi 
+
+
+if [ -e /mnt/backup/jout14.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour14.db
+
+fi 
+
+
+if [ -e /mnt/backup/jour15.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour15.db
+
+fi 
+
+
+
+if [ -e /mnt/backup/jour16.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour16.db
+
+fi 
+
+
+if [ -e /mnt/backup/jour17.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour17.db
+
+fi 
+
+
+
+if [ -e /mnt/backup/jour18.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour18.db
+
+fi 
+
+
+
+if [ -e /mnt/backup/jour19.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour19.db
+
+fi 
+
+
+
+if [ -e /mnt/backup/jour20.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour20.db
+
+fi 
+
+
+
+if [ -e /mnt/backup/jour21.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour21.db
+
+fi 
+
+
+if [ -e /mnt/backup/jour22.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour22.db
+
+fi 
+
+
+
+if [ -e /mnt/backup/jour23.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour23.db
+
+fi 
+
+
+
+if [ -e /mnt/backup/jour24.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour24.db
+
+fi 
+
+
+
+if [ -e /mnt/backup/jour25.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour25.db
+
+fi 
+
+
+
+if [ -e /mnt/backup/jour26.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour26.db
+
+fi 
+
+
+
+if [ -e /mnt/backup/jour27.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour27.db
+
+fi 
+
+
+
+
+if [ -e /mnt/backup/jour28.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour28.db
+
+fi 
+
+
+
+
+if [ -e /mnt/backup/jour29.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour29.db
+
+fi 
+
+
+
+if [ -e /mnt/backup/jour30.db ]; then
+       echo "file found!"
+
+else
+       echo "Téléchargement de la base..."
+       curl -s http://192.168.1.1:8080/backupdatabase.php > /mnt/backup/jour30.db
+
+fi 
+find mnt/backup/lundi.db -mtime +15 rm {} \;
+
+
+
+
+
+
